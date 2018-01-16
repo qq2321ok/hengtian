69b3dd99e11c84b5f7940e4f2ebfc946cad7a11d
wujw <jiaweiandroidsuc@gmail.com>
2018-01-16 10:01:39 +0800

    init

---
+++ company/src/test/java/com/wujw/hengtian/company/CompanyApplicationTests.java
@@ -0,0 +1,12 @@
+package com.wujw.hengtian.company;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class CompanyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
