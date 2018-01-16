b4ad41f3ac11124bcc288826741808a5b7eacb9a
wujw <jiaweiandroidsuc@gmail.com>
2018-01-16 10:01:02 +0800

    init

---
+++ factory/src/test/java/com/wujw/hengtian/factory/FactoryApplicationTests.java
@@ -0,0 +1,12 @@
+package com.wujw.hengtian.factory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class FactoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
